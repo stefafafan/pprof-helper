7fd747905d446a11c089edb5057547b496e3c9d1
Stefan Utamaru <mail@stenyan.jp>
2023-01-08 17:32:10 +0900

    parse framework

--- main.go
+++ main.go
@@ -8,20 +8,50 @@ import (
 	"golang.org/x/mod/modfile"
 )
 
+const (
+	ECHO_V4 = "echo_v4"
+	ECHO    = "echo"
+)
+
+var frameworks = map[string]string{
+	"github.com/labstack/echo/v4": ECHO_V4,
+	"github.com/labstack/echo":    ECHO,
+}
+
 func main() {
 	var goModFile string
 	flag.StringVar(&goModFile, "go-mod-file", "go.mod", "specify the path of the go.mod file")
 	flag.Parse()
 
-	goModBytes, err := os.ReadFile(goModFile)
+	require, err := parsePackages(goModFile)
 	if err != nil {
 		return
 	}
-	f, err := modfile.Parse(goModFile, goModBytes, nil)
+
+	framework := getFramework(require)
+	if framework != "" {
+		fmt.Println(framework)
+	}
+}
+
+func parsePackages(path string) ([]*modfile.Require, error) {
+	goModBytes, err := os.ReadFile(path)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	for _, v := range f.Require {
-		fmt.Println(v.Mod)
+	f, err := modfile.Parse(path, goModBytes, nil)
+	if err != nil {
+		return nil, err
 	}
+	return f.Require, nil
+}
+
+func getFramework(requires []*modfile.Require) string {
+	for _, req := range requires {
+		foo := frameworks[req.Mod.Path]
+		if foo != "" {
+			return foo
+		}
+	}
+	return ""
 }
